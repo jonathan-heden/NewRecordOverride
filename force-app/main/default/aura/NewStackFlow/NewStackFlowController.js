e97dbc67c72787ab2cb7a3dee03b044ddffaa7dd
jonathan-heden <jonathan.heden@fluidogroup.com>
2024-08-13 08:56:08 +0200

    WebHook handler and listener classes

--- force-app/main/default/aura/NewStackFlow/NewStackFlowController.js
+++ force-app/main/default/aura/NewStackFlow/NewStackFlowController.js
@@ -10,12 +10,15 @@
             var state = response.getState();
             if(state == "SUCCESS") {
                 var recordTypeName = response.getReturnValue();
-                var header = component.find("header").set("v.value", "New Stack (" + recordTypeName + ")");
+                var header = component.find("header").set("v.value", "New Stack: " + recordTypeName);
             }
         });
         $A.enqueueAction(actionGetRecordTypeName);
         //***********************************
         var actionGetLayoutFieldNames = component.get("c.getLayoutFieldNames");
+        actionGetLayoutFieldNames.setParams({
+            recordTypeId: recordTypeId
+        });
         actionGetLayoutFieldNames.setCallback(this, function(response) {
             var state = response.getState();
             if(state == "SUCCESS") {
@@ -24,6 +27,33 @@
             }
         });
         $A.enqueueAction(actionGetLayoutFieldNames);
+        //***********************************
+        var actionGetSection = component.get("c.getLayoutSections");
+        actionGetSection.setParams({
+            recordTypeId: recordTypeId
+        });
+        actionGetSection.setCallback(this, function(response) {
+            var state = response.getState();
+            if(state == "SUCCESS") {
+                var sections = response.getReturnValue();
+                component.set("v.sections", sections);
+            }
+        });
+        $A.enqueueAction(actionGetSection);
+        //***********************************
+        var actionGetLayoutFields = component.get("c.getLayoutFields");
+        actionGetLayoutFields.setParams({
+            recordTypeId: recordTypeId
+        });
+        actionGetLayoutFields.setCallback(this, function(response) {
+            var state = response.getState();
+            if(state == "SUCCESS") {
+                var fields = response.getReturnValue();
+                console.log(JSON.stringify(fields));
+                component.set("v.fieldSet", fields);
+            }
+        });
+        $A.enqueueAction(actionGetLayoutFields);
         //***********************************
         /*
         Find the component whose aura:id is "flowData"
@@ -41,5 +71,27 @@
      
     handleCloseModal: function(component, event, helper) {
         window.history.back();
+    },
+    
+    handleSave: function(component, event, helper) {
+        component.find("recordEditForm").submit();
+    },
+    
+    handleSuccess: function(component, event, helper) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": "Success!",
+            "message": "Stack has been created successfully.",
+            "type": "success"
+        });
+        toastEvent.fire();
+        
+        var params = event.getParams();
+        var recordId = params.response.id;
+        var navEvent = $A.get("e.force:navigateToSObject");
+        navEvent.setParams({
+          "recordId": recordId
+        });
+        navEvent.fire();
     }
 })
